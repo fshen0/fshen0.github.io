2824e56f0c6aee4b216675d2c3fcb020cbdbabec
fshen0 <fshen5206@gmail.com>
2022-07-31 01:01:43 +0800

    Site updated: 2022-07-31 01:01:43

---
+++ js/switch.js
@@ -0,0 +1,24 @@
+function switchNightMode() {
+"true" == localStorage.getItem("light")
+    ? (localStorage.setItem("light", "false"),switchToDark()) 
+    : (localStorage.setItem("light", "true"), switchToLight());
+}
+
+function switchToDark() {
+    for (var t = document.getElementsByTagName("link"), e = 0; e < t.length; e++)
+        t[e] &&
+        t[e].href &&
+        -1 != t[e].href.indexOf("/css/style-light.css") &&
+        t[e].setAttribute("href", "/css/style.css");
+}
+function switchToLight() {
+    for (var t = document.getElementsByTagName("link"), e = 0; e < t.length; e++)
+        t[e] &&
+        t[e].href &&
+        -1 != t[e].href.indexOf("/css/style.css") &&
+        t[e].setAttribute("href", "/css/style-light.css");
+}
+null == localStorage.getItem("light")
+? localStorage.setItem("light", "true")
+: ("false" == localStorage.getItem("light") ? switchToDark : switchToLight)();
+
