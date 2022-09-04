8dd4530a0f012ddba4beaa93e67d28ad8412e068
fshen0 <fshen5206@gmail.com>
2022-09-05 02:11:31 +0800

    Site updated: 2022-09-05 02:11:31

--- js/main.js
+++ js/main.js
@@ -117,3 +117,33 @@ $(document).ready(function() {
   //   document.location.href="/"
   // });
 });
+
+//switch theme
+function switchNightMode() {
+  $("#loading-box").fadeIn();
+  "true" == localStorage.getItem("light")
+      ? (localStorage.setItem("light", "false"),switchToDark()) 
+      : (localStorage.setItem("light", "true"), switchToLight());
+  // setTimeout("loadingout()", 0)
+  $("#loading-box").fadeOut();
+}
+// function loadingout(){
+//     $("#loading-box").fadeOut();
+// }
+function switchToDark() {
+  for (var t = document.getElementsByTagName("link"), e = 0; e < t.length; e++)
+      t[e] &&
+      t[e].href &&
+      -1 != t[e].href.indexOf("/css/style-light.css") &&
+      t[e].setAttribute("href", "/css/style.css");
+}
+function switchToLight() {
+  for (var t = document.getElementsByTagName("link"), e = 0; e < t.length; e++)
+      t[e] &&
+      t[e].href &&
+      -1 != t[e].href.indexOf("/css/style.css") &&
+      t[e].setAttribute("href", "/css/style-light.css");
+}
+null == localStorage.getItem("light")
+? localStorage.setItem("light", "false")
+: ("true" == localStorage.getItem("light") ? switchToLight: switchToDark)();
